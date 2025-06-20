eedb5388e1c322e98e4459198e26ed8f18d374af
金　兌訓 <kim.taehoon@ecbeing.co.jp>
2025-06-20 19:49:28 +0900

    テスト

--- server.js
+++ server.js
@@ -3793,7 +3793,7 @@ app.get('/styles.css', (req, res) => {
         .inkan-image {
             position: absolute;
             right: -25px;   /* 署名テキストより右へ */
-            top: -10px;     /* 少し上に配置 */
+            top: 10px;     /* 少し上に配置 */
             display: inline-block;
             width: 20px;
             height: 20px;
