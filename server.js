9eb71e67ed12f2658c7575a3df2ea49b18e9bc28
KIM TAEHOON <xogns00089@gmail.com>
2025-06-27 00:34:13 +0900

    test

--- server.js
+++ server.js
@@ -61,6 +61,7 @@ transporter.sendMail({
   
 // MongoDB接続
 const MONGODB_URI = process.env.MONGODB_URI;
+console.log('MONGODB_URI:', MONGODB_URI);
 mongoose.connect(MONGODB_URI)
   .then(() => console.log('MongoDB接続成功'))
   .catch(err => console.error('MongoDB接続エラー:', err));
